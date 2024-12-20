bb7078f9a897e0319aa7025b769094232f8b0bfd
Alexander Ortiz <CL198785156@enel.com>
2024-12-20 18:56:55 -0300

    add login and register page

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,8 +1,42 @@
+import "@fontsource-variable/onest";
 import Link from "next/link";
+import { useHeader } from "./hooks/useHeader";
 
 const Header = () => {
+  const {
+    //* Variables
+    menuOpen,
+    router,
+    showModal,
+    messageModal,
+    username,
+    isDropdownOpen,
+    isClickCloseSesion,
+
+    //* Functions
+    onClickLogo,
+    onClickToggleMenu,
+    onClickCloseModal,
+    toggleDropdown,
+    onClickCloseSession,
+    onClickAcceptModal,
+  } = useHeader();
+
   return (
     <>
+      {showModal && (
+        <>
+          <div className="fixed inset-0 bg-gray-600 bg-opacity-50 z-10"></div>
+          <div className="z-20 fixed inset-0 flex items-center justify-center">
+            {/* <WarningModal
+              message={messageModal}
+              onClickClose={onClickCloseModal}
+              okButton={isClickCloseSesion}
+              onClickAccept={onClickAcceptModal}
+            /> */}
+          </div>
+        </>
+      )}
       <div className="fixed w-full top-0 z-50 border-b">
         <header className="bg-white w-full h-12 text-black flex justify-between items-center px-4">
           <div>
@@ -15,52 +49,75 @@ const Header = () => {
             /> */}
           </div>
           <div className="md:hidden w-full md:w-auto flex justify-end items-end">
-            <li className="md:hidden flex items-center justify-center w-[32px] h-[32px] relative cursor-pointer z-50">
+            <li
+              onClick={onClickToggleMenu}
+              className="md:hidden flex items-center justify-center w-[32px] h-[32px] relative cursor-pointer z-50"
+            >
               <span
-                className={`absolute block w-[24px] h-[2px] bg-black transition-transform duration-300 ease-in-out`}
+                className={`absolute block w-[24px] h-[2px] bg-black transition-transform duration-300 ease-in-out ${
+                  menuOpen ? "rotate-45" : "-translate-y-[4px]"
+                }`}
               ></span>
               <span
-                className={`absolute block w-[24px] h-[2px] bg-black transition-transform duration-300 ease-in-out`}
+                className={`absolute block w-[24px] h-[2px] bg-black transition-transform duration-300 ease-in-out ${
+                  menuOpen ? "-rotate-45" : "translate-y-[4px]"
+                }`}
               ></span>
             </li>
           </div>
           <div>
             <nav
-              className={`fixed inset-0 bg-white flex flex-col items-center justify-center transition-transform transform md:relative md:translate-x-0 md:flex-row md:items-center md:justify-end md:h-auto md:bg-transparent`}
+              className={`fixed inset-0 bg-white flex flex-col items-center justify-center transition-transform transform ${
+                menuOpen ? "translate-x-0" : "translate-x-full"
+              } md:relative md:translate-x-0 md:flex-row md:items-center md:justify-end md:h-auto md:bg-transparent`}
             >
               <Link
                 href="/about"
-                className={`px-4 mb-4 md:mb-0 text-center font-bold text-lg md:text-xs relative after:content-[''] after:absolute after:bottom-[-4px] after:left-0 after:w-0 after:h-[2px] after:bg-blue-500 after:transition-all after:duration-300 after:ease-in-out hover:after:w-full`}
+                className={`px-4 mb-4 md:mb-0 text-center font-bold text-lg md:text-xs relative after:content-[''] after:absolute after:bottom-[-4px] after:left-0 after:w-0 after:h-[2px] after:bg-blue-500 after:transition-all after:duration-300 after:ease-in-out hover:after:w-full ${
+                  router.pathname === "/about" ? "after:w-full" : "after:w-0"
+                }`}
               >
                 Sobre nosotros
               </Link>
               <Link
                 href="/products"
-                className={`px-4 mb-4 md:mb-0 text-center font-bold text-lg md:text-xs relative after:content-[''] after:absolute after:bottom-[-4px] after:left-0 after:w-0 after:h-[2px] after:bg-blue-500 after:transition-all after:duration-300 after:ease-in-out hover:after:w-full`}
+                className={`px-4 mb-4 md:mb-0 text-center font-bold text-lg md:text-xs relative after:content-[''] after:absolute after:bottom-[-4px] after:left-0 after:w-0 after:h-[2px] after:bg-blue-500 after:transition-all after:duration-300 after:ease-in-out hover:after:w-full ${
+                  router.pathname === "/products" ? "after:w-full" : "after:w-0"
+                }`}
               >
                 Nuestros productos
               </Link>
               <Link
                 href="/locations"
-                className={`px-4 mb-4 md:mb-0 text-center font-bold text-lg md:text-xs relative after:content-[''] after:absolute after:bottom-[-4px] after:left-0 after:w-0 after:h-[2px] after:bg-blue-500 after:transition-all after:duration-300 after:ease-in-out hover:after:w-full `}
+                className={`px-4 mb-4 md:mb-0 text-center font-bold text-lg md:text-xs relative after:content-[''] after:absolute after:bottom-[-4px] after:left-0 after:w-0 after:h-[2px] after:bg-blue-500 after:transition-all after:duration-300 after:ease-in-out hover:after:w-full ${
+                  router.pathname === "/locations"
+                    ? "after:w-full"
+                    : "after:w-0"
+                }`}
               >
                 Nuestras tiendas
               </Link>
               <Link
                 href="/contact"
-                className={`px-4 mb-4 md:mb-0 text-center font-bold text-lg md:text-xs relative after:content-[''] after:absolute after:bottom-[-4px] after:left-0 after:w-0 after:h-[2px] after:bg-blue-500 after:transition-all after:duration-300 after:ease-in-out hover:after:w-full`}
+                className={`px-4 mb-4 md:mb-0 text-center font-bold text-lg md:text-xs relative after:content-[''] after:absolute after:bottom-[-4px] after:left-0 after:w-0 after:h-[2px] after:bg-blue-500 after:transition-all after:duration-300 after:ease-in-out hover:after:w-full ${
+                  router.pathname === "/contact" ? "after:w-full" : "after:w-0"
+                }`}
               >
                 Contactanos
               </Link>
-              {/* {username === "" && (
+              {username === "" && (
                 <Link
                   href="/register"
-                  className={`px-4 mb-2 md:mb-0 text-center font-bold text-lg md:text-xs relative after:content-[''] after:absolute after:bottom-[-4px] after:left-0 after:w-0 after:h-[2px] after:bg-blue-500 after:transition-all after:duration-300 after:ease-in-out hover:after:w-full`}
+                  className={`px-4 mb-2 md:mb-0 text-center font-bold text-lg md:text-xs relative after:content-[''] after:absolute after:bottom-[-4px] after:left-0 after:w-0 after:h-[2px] after:bg-blue-500 after:transition-all after:duration-300 after:ease-in-out hover:after:w-full ${
+                    router.pathname === "/register"
+                      ? "after:w-full"
+                      : "after:w-0"
+                  }`}
                 >
                   Registro
                 </Link>
-              )} */}
-              {/* {username === "" ? (
+              )}
+              {username === "" ? (
                 <Link
                   href="/login"
                   className="md:ml-4 text-lg md:text-xs text-center cursor-pointer font-bold md:mr-5 md:text-white md:bg-blue-500 md:hover:bg-blue-600 h-12 md:h-8 flex justify-center items-center md:p-2 md:rounded-[9999px]  transition-all ease-in-out"
@@ -112,7 +169,7 @@ const Header = () => {
                     </div>
                   )}
                 </div>
-              )} */}
+              )}
             </nav>
           </div>
         </header>
---
+++ src/pages/login.js
@@ -0,0 +1,142 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import Link from "next/link";
+
+const Login = () => {
+  return (
+    <div className="app-container relative">
+      <Header />
+
+      <section className="bg-gray-200 h-screen">
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto h-full lg:py-0">
+          <div className="w-full rounded-lg shadow border md:mt-0 sm:max-w-md xl:p-0  bg-gray-900">
+            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 className="text-xl font-bold leading-tight tracking-tight text-white md:text-2xl">
+                Inicia sesión con tu cuenta
+              </h1>
+              <form
+                className="space-y-4 md:space-y-6" //@ts-ignore
+              >
+                <div>
+                  <label className="block mb-2 text-sm font-medium text-white">
+                    Ingresa tu correo
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    className="bg-gray-700 border text-white rounded-lg block w-full p-2.5 border-gray-600 outline-none"
+                    placeholder="correo@correo.com"
+                  />
+                  {/*   {errors.email && (
+                    <p className="text-red-200 mt-3">{errors.email}</p>
+                  )} */}
+                </div>
+                <div className="relative">
+                  <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                    Ingresa tu contraseña
+                  </label>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="absolute top-9 right-5 size-6 text-white cursor-pointer"
+                  >
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M5 13a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v6a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2v-6z" />
+                    <path d="M11 16a1 1 0 1 0 2 0a1 1 0 0 0 -2 0" />
+                    <path d="M8 11v-4a4 4 0 1 1 8 0v4" />
+                  </svg>
+                  {/* {showPassword ? (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      onClick={onClickShowPassword}
+                      className="absolute top-9 right-5 size-6 text-white cursor-pointer"
+                    >
+                      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                      <path d="M5 11m0 2a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v6a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2z" />
+                      <path d="M12 16m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
+                      <path d="M8 11v-5a4 4 0 0 1 8 0" />
+                    </svg>
+                  ) : (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      onClick={onClickShowPassword}
+                      className="absolute top-9 right-5 size-6 text-white cursor-pointer"
+                    >
+                      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                      <path d="M5 13a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v6a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2v-6z" />
+                      <path d="M11 16a1 1 0 1 0 2 0a1 1 0 0 0 -2 0" />
+                      <path d="M8 11v-4a4 4 0 1 1 8 0v4" />
+                    </svg>
+                  )} */}
+                  <input
+                    name="password"
+                    type="password"
+                    id="password"
+                    placeholder="••••••••"
+                    className="bg-gray-700 border text-white rounded-lg block w-full p-2.5 border-gray-600 outline-none"
+                  />
+                  {/* {errors.password && (
+                    <p className="text-red-200 mt-3">{errors.password}</p>
+                  )} */}
+                </div>
+                <div className="flex items-center justify-between">
+                  <a
+                    href="#"
+                    className="text-sm font-medium text-blue-200 hover:underline hover:cursor-pointer"
+                  >
+                    ¿Olvidaste tu contraseña?
+                  </a>
+                </div>
+                <button
+                  type="submit"
+                  className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                >
+                  Iniciar sesión
+                </button>
+
+                <p className="text-sm font-light text-gray-400">
+                  No tienes una cuenta?
+                  <Link
+                    href="/register"
+                    className="font-medium text-primary-500 hover:underline "
+                  >
+                    {" "}
+                    Resgistrarme
+                  </Link>
+                </p>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default Login;
